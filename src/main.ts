8ec8f62bac33395e443cb3e87733f5f0433c068e
Vitoriano <vitorianoernandes2012@gmail.com>
2022-07-11 21:05:44 -0300

    sync

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import Address from './entity/address';
-import Customer from './entity/customer'
-import Order from './entity/order';
-import OrderItem from './entity/order_item';
+// import Address from './entity/address';
+// import Customer from './entity/customer'
+// import Order from './entity/order';
+// import OrderItem from './entity/order_item';
 
-let customer = new Customer('123', "Vitoriano");
-const address = new Address("Rua dois", "123", "59290-000", "Natal");
+// let customer = new Customer('123', "Vitoriano");
+// const address = new Address("Rua dois", "123", "59290-000", "Natal");
 
-customer.setAddress(address);
-customer.activate();
+// customer.setAddress(address);
+// customer.activate();
 
-const item1 = new OrderItem("1", "Item1", 10);
-const item2 = new OrderItem("2", "Item2", 15);
+// const item1 = new OrderItem("1", "Item1", 10);
+// const item2 = new OrderItem("2", "Item2", 15);
 
-const order = new Order("1", "123",[item1, item2]);
+// const order = new Order("1", "123",[item1, item2]);
